bcdc5167cdc3d1c442a5c8d46223ac6a0a3b672f
honjiaxuan <honjiaxuan@users.noreply.github.com>
2017-02-22 06:30:10 +0800

    Update autoStart.java

--- autoStart.java
+++ autoStart.java
@@ -3,10 +3,7 @@ package com.gps.detect;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.provider.Telephony;
-import android.telephony.SmsMessage;
-import android.telephony.TelephonyManager;
-import android.text.format.DateFormat;
+import android.location.LocationManager;
 import android.util.Log;
 
 
